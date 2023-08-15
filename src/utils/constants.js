75c02a93cec3dc6ad08ad220fd7920b58d70f6a1
mikasa404 <jhapriya106@gmail.com>
2023-08-15 16:53:20 +0530

    updates

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,2 @@
 const key= process.env.REACT_APP_GOOGLE_API_KEY;
-console.log(key)
 export const YOUTUBE_VIDEOS_API ="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key="+key;
-console.log(YOUTUBE_VIDEOS_API);
