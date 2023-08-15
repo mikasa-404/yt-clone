d2ab76ec0e76ca46cdecff611dba4d3c6fdf475e
mikasa404 <jhapriya106@gmail.com>
2023-08-15 16:50:12 +0530

    added env variables

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
-import config from "../config"
-
-export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+ config.GOOGLE_API_KEY ;
+const key= process.env.REACT_APP_GOOGLE_API_KEY;
+console.log(key)
+export const YOUTUBE_VIDEOS_API ="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key="+key;
+console.log(YOUTUBE_VIDEOS_API);
