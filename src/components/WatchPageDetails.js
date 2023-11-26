4ec9580e40e91816e86aff8de43b5f5b6ea6c559
mikasa404 <jhapriya106@gmail.com>
2023-11-26 22:29:34 +0530

    build issues resolved

--- src/components/WatchPageDetails.js
+++ src/components/WatchPageDetails.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useEffect , useState} from 'react';
+import { useEffect} from 'react';
 import { VIDEO_API } from '../utils/constants';
 
 const WatchPageDetails = (videoId) => {
