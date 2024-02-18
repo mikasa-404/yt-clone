30a5c95b876dc90d98a6ed4a372ce1a734f79e7e
Priya Jha <jhapriya106@gmail.com>
2024-02-18 20:47:17 +0530

    searchSuggestions improved

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState, useRef } from "react";
 import ytlogo from "../imgs/youtube_logo.jpg";
 import hamButton from "../imgs/png-transparent-hamburger-button-computer-icons-menu-ganesha-angle-white-food.png";
-import searchButton from "../imgs/search.png"
-import userIcon from "../imgs/user.png"
+import searchButton from "../imgs/search.png";
+import userIcon from "../imgs/user.png";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
-import { YOUTUBE_SEARCH_API } from "../utils/constants";
 import { cacheResults } from "../utils/searchSlice";
 import { Link } from "react-router-dom";
 
@@ -20,12 +19,12 @@ const Header = () => {
   const dispatch = useDispatch();
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
-  }
+  };
 
   useEffect(() => {
     const timer = setTimeout(() => {
       if (Scache[searchQuery]) {
-        setSearchSuggestions(Scache[searchQuery])
+        setSearchSuggestions(Scache[searchQuery]);
       } else {
         getSearchSuggestions();
       }
@@ -48,7 +47,9 @@ const Header = () => {
   }, [suggestionsRef]);
 
   const getSearchSuggestions = async () => {
-    const data = await fetch(`https://yt-server-zum8.onrender.com/api/data/${searchQuery}`);
+    const data = await fetch(
+      `https://yt-server-zum8.onrender.com/api/data/${searchQuery}`
+    );
     const json = await data.json();
     setSearchSuggestions(json[1]);
 
@@ -58,42 +59,67 @@ const Header = () => {
         [searchQuery]: json[1],
       })
     );
-  }
+  };
   return (
     <div className="grid grid-flow-col ">
       <div className="1 flex m-1 col-span-2">
-        <img className="h-8 m-2 p-1 cursor-pointer" src={hamButton} alt="hambutton" onClick={() => toggleMenuHandler()} ></img>
+        <img
+          className="h-8 m-2 p-1 cursor-pointer"
+          src={hamButton}
+          alt="hambutton"
+          onClick={() => toggleMenuHandler()}
+        ></img>
         <a href="/">
           <img className="h-8 m-2" alt="logo" src={ytlogo}></img>
         </a>
       </div>
-      <div ref={suggestionsRef} className="m-1 col-span-6 ">
+      <div ref={suggestionsRef} className="m-1 col-span-6 relative">
         <div className="w-full flex items-center">
-          <input className=" w-8/12 border-slate-300 border rounded-l-2xl m-0 mr-0 text-base h-9 px-4 focus:outline-none focus:shadow-outline focus:border-blue-700"
+          <input
+            className=" w-8/12 border-slate-300 border rounded-l-2xl m-0 mr-0 text-base h-9 px-4 focus:outline-none focus:shadow-outline focus:border-blue-700"
             placeholder="Search"
             value={searchQuery}
             onChange={(e) => {
-              return setSearchQuery(e.target.value)
+              return setSearchQuery(e.target.value);
             }}
             onClick={() => setShowSuggestion(true)}
           ></input>
-          <button className="border-slate-300 border rounded-r-2xl ml-0 p-2 h-9 hover:bg-gray-100">
-            <img className=" h-5 pl-3 pr-4" src={searchButton} alt="searchbtn"></img>
-          </button>
+          <Link to={"/results/" + searchQuery}>
+            <button className="border-slate-300 border rounded-r-2xl ml-0 p-2 h-9 hover:bg-gray-100">
+              <img
+                className=" h-5 pl-3 pr-4"
+                src={searchButton}
+                alt="searchbtn"
+              ></img>
+            </button>
+          </Link>
         </div>
-        {showSuggestions && <div className="bg-white fixed w-2/5 rounded-2xl ">
-          <ul className="">
-            {searchSuggestions.map((s) => (
-            <Link to={"/results/"+s} key={s} >
-              <li className="hover:bg-gray-200 px-4 py-1">{s}</li>
-            </Link>
-            ))}
-          </ul>
-        </div>}
+        {showSuggestions && (
+          <div className="bg-white absolute w-4/5 rounded-2xl z-10">
+            {searchSuggestions.length !== 0 ? (
+              <ul className="">
+                {searchSuggestions.map((s, index) => (
+                  <Link to={"/results/" + s} key={index}>
+                    <li className="hover:bg-gray-200 px-4 py-1">{s}</li>
+                  </Link>
+                ))}
+              </ul>
+            ) : (
+              <div className=" text-red-700 m-2">
+                {" "}
+                Loading suggestions... this might take up a minute
+              </div>
+            )}
+          </div>
+        )}
       </div>
 
       <div className="3 m-1  col-span-2">
-        <img className="h-8 m-2 float-right mr-5 " src={userIcon} alt="userLogo"></img>
+        <img
+          className="h-8 m-2 float-right mr-5 "
+          src={userIcon}
+          alt="userLogo"
+        ></img>
       </div>
     </div>
   );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 const key= process.env.REACT_APP_GOOGLE_API_KEY;
 export const YOUTUBE_VIDEOS_API ="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key="+key;
-export const VIDEO_SEARCH_API="https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key="+key+"&q="
+export const VIDEO_SEARCH_API="https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&key="+key+"&q="
 
 export const VIDEO_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key=" + key;
 export const OFFSET_LIVE_CHAT=35;
